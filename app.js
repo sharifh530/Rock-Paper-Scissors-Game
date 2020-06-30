4077ca51625da1b8258214bf0921ea7a8b10c6b0
SHARIF <sharifh530@gmail.com>
2020-06-30 18:34:32 +0600

    The game sucssesfully Works

---
+++ app.js
@@ -0,0 +1,107 @@
+
+let userScore =0;
+let computerScore=0;
+
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div=document.querySelector(".score-board");
+const result_p =document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+
+function getComputerChoice(){
+    const choices= ['r','p','s'];
+    const randomNumber =Math.floor(Math.random()*3);
+    return choices[randomNumber];
+}
+
+function wordChanger(word){
+    
+    if(word === "r"){
+        return "Rock";
+    }
+    else if(word=== "p"){
+        return "Paper";
+    }
+    else if(word=== "s"){
+        return "Scissors";
+    }
+
+}
+
+function win(userChoice, computerChoice){
+    userScore++;
+    userScore_span.innerHTML=userScore;
+    computerScore_span.innerHTML=computerScore;
+    const smallUserWord ="user".fontsize(2).sub();
+    const smallCompWord ="comp".fontsize(2).sub();
+    result_p.innerHTML= `${wordChanger(userChoice)}${smallUserWord}   beats  ${wordChanger(computerChoice)}${smallCompWord} You Win! 👌`;
+}
+
+function lose(userChoice, computerChoice){
+    computerScore++;
+    userScore_span.innerHTML=userScore;
+    computerScore_span.innerHTML=computerScore;
+    const smallUserWord ="user".fontsize(2).sub();
+    const smallCompWord ="comp".fontsize(2).sub();
+    result_p.innerHTML= `${wordChanger(userChoice)}${smallUserWord}  Can't beat  ${wordChanger(computerChoice)}${smallCompWord} You Lose! 😂`;
+}
+
+function draw(userChoice, computerChoice){
+    const smallUserWord ="user".fontsize(2).sub();
+    const smallCompWord ="comp".fontsize(2).sub();
+    result_p.innerHTML= `${wordChanger(userChoice)}${smallUserWord}   Equals   ${wordChanger(computerChoice)}${smallCompWord} It's A Draw! 😆`;
+}
+
+
+
+
+
+function game(userChoice){
+
+    const computerChoice = getComputerChoice();
+    console.log(userChoice);
+    console.log(computerChoice);
+
+    switch (userChoice+computerChoice){
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+    }
+    
+}
+
+
+
+
+
+
+function main(){
+    rock_div.addEventListener('click', function(){
+        game('r');
+    })
+
+    paper_div.addEventListener('click', function(){
+        game('p');
+    })
+
+    scissors_div.addEventListener('click', function(){
+        game('s');
+    })
+}
+
+main();
